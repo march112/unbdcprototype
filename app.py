6f483000cbb01785879b72ede5df78e5748edc24
MarcusC <marcus.a.chung@gmail.com>
2021-05-31 16:31:01 -0400

    added random forest and complement

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import numpy as np
 from flask import Flask, render_template,request
 import pickle#Initialize the flask App
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-counter = pickle.load(open('counter.pkl', 'rb'))
+modelmn = pickle.load(open('model.pkl', 'rb'))
+countermn = pickle.load(open('counter.pkl', 'rb'))
+modelcm = pickle.load(open('modelcomp.pkl', 'rb'))
+countercm = pickle.load(open('countercomp.pkl', 'rb'))
+modelrf = pickle.load(open('modelrf.pkl', 'rb'))
+counterrf = pickle.load(open('rfcounter.pkl', 'rb'))
 
 #default page of our web-app
 @app.route('/')
@@ -15,11 +19,17 @@ def home():
 def predict():
     #For rendering results on HTML GUI
     words = [x for x in request.form.values()]
-    pred_counts = counter.transform(words)
-    prediction = model.predict(pred_counts) 
-    #output = lambda x: "Real" if x == 0 else "Fake"
-    #final = output(prediction)
-    return render_template('index.html', prediction_text=' Our Naive Bayes Model predicts your tweet is :{}'.format(prediction))
+    pred1_counts = countermn.transform(words)
+    prediction1 = modelmn.predict(pred1_counts)
+    pred2_counts = countercm.transform(words)
+    prediction2 = modelcm.predict(pred2_counts) 
+    pred3_counts = counterrf.transform(words)
+    prediction3 = modelrf.predict(pred3_counts)  
+    output = lambda x: "Real" if x == 0 else "Fake"
+    final1 = output(prediction1)
+    final2 = output(prediction2)
+    final3 = output(prediction3)
+    return render_template('index.html', prediction_text=' Our Naive Bayes Multinomial Model predicts your tweet is {0}. Our Naive Bayes Complement Model predicts your tweet is {1}. Random Forest Model predicts your tweet is {2}.'.format(final1, final2, final3))
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ complement.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import ComplementNB
+import pickle
+
+df = pd.read_csv("datapreprocessed.csv")
+df = df[["text", "label"]]
+#print(df.head())
+df["label"] = df["label"].map({'real': 0, 'fake':1})
+#print(df["label"].unique())
+df = df.dropna(axis=0, how='any')
+X = df["text"].tolist()
+y = df["label"].tolist()
+
+X_train, X_test = train_test_split(X, test_size = 0.3, random_state=1)
+y_train, y_test = train_test_split(y, test_size=0.3, random_state=1)
+
+cv = CountVectorizer()
+cv.fit(X_train)
+X_train = cv.transform(X_train) 
+X_test = cv.transform(X_test) 
+feature_names = cv.get_feature_names()
+
+model = ComplementNB()
+model.fit(X_train, y_train)
+
+pickle.dump(cv, open('countercomp.pkl', 'wb'))
+pickle.dump(model, open('modelcomp.pkl','wb'))
