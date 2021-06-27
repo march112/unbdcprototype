4deba472758ddeb57acd46721322b13209c03ad5
MarcusC <marcus.a.chung@gmail.com>
2021-06-27 16:26:43 -0700

    Somewhat fixed the model. Only using nb and comp right now.

--- complement.py
+++ complement.py
@@ -2,29 +2,38 @@ import pandas as pd
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import ComplementNB, MultinomialNB
 import pickle
+import nltk
+nltk.download("stopwords", quiet=True) # helps us get rid of stop words
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 df = pd.read_csv("text_preprocessed.csv")
-df = df[["text", "label"]]
-#print(df.head())
-df["label"] = df["label"].map({'real': 0, 'fake':1})
-#print(df["label"].unique())
-df = df.dropna(axis=0, how='any')
-X = df["text"].tolist()
-y = df["label"].tolist()
+df = df.iloc[:,1:]
+# df = df[["text", "label"]]
+# print(df.head())
+# df["label"] = df["label"].map({'real': 0, 'fake':1})
+# # #print(df["label"].unique())
+# df = df.dropna(axis=0, how='any')
+# X = df["text"].tolist()
+# y = df["label"].tolist()
+# # df.loc[df.label == 1,:].head()
+X = df.text
+y = df.label
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, train_size=0.75,random_state=0)
 
-X_train, X_test = train_test_split(X, test_size = 0.3, random_state=1)
-y_train, y_test = train_test_split(y, test_size=0.3, random_state=1)
-
-cv = CountVectorizer()
+#X_train, X_val, y_train, y_val = train_test_split(X_train,y_train, test_size=0.25, random_state=0)
+cv = CountVectorizer(stop_words=stopwords.words("english"))
 cv.fit(X_train)
 X_train = cv.transform(X_train) 
 X_test = cv.transform(X_test) 
-feature_names = cv.get_feature_names()
+# feature_names = cv.get_feature_names()
 
 model = ComplementNB()
 model.fit(X_train, y_train)
 
-pickle.dump(cv, open('countercomp.pkl', 'wb'))
-pickle.dump(model, open('modelcomp.pkl','wb'))
+
+pickle.dump(cv, open('countercm.pkl', 'wb'))
+pickle.dump(model, open('modelcm.pkl','wb'))
+
--- nb.py
+++ nb.py
@@ -2,26 +2,30 @@ import pandas as pd
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import ComplementNB, MultinomialNB
 import pickle
+import nltk
+nltk.download("stopwords", quiet=True) # helps us get rid of stop words
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 df = pd.read_csv("text_preprocessed.csv")
-df = df[["text", "label"]]
-#print(df.head())
-df["label"] = df["label"].map({'real': 0, 'fake':1})
-#print(df["label"].unique())
-df = df.dropna(axis=0, how='any')
-X = df["text"].tolist()
-y = df["label"].tolist()
+df = df.iloc[:,1:]
+X = df.text
+y = df.label
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, train_size=0.75,random_state=0)
 
-#X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=0.2, random_state=1)
+#X_train, X_val, y_train, y_val = train_test_split(X_train,y_train, test_size=0.25, random_state=0)
+cv = CountVectorizer(stop_words=stopwords.words("english"))
+cv.fit(X_train)
+X_train = cv.transform(X_train) 
+X_test = cv.transform(X_test) 
+# feature_names = cv.get_feature_names()
 
-counter = CountVectorizer()
-counter.fit(X)
-counts = counter.transform(X)
-classifier = MultinomialNB()
-classifier.fit(counts, y)
-pickle.dump(counter, open('counter.pkl', 'wb'))
-pickle.dump(classifier, open('model.pkl','wb'))
-# test_counts = counter.transform("Vaccines suck")
-# print(classifier.predict(["Vaccines suck"]))
+model = MultinomialNB()
+model.fit(X_train, y_train)
+y_test_pred = model.predict(X_test)
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_test_pred))
+# pickle.dump(cv, open('counternb.pkl', 'wb'))
+# pickle.dump(model, open('modelnb.pkl','wb'))
